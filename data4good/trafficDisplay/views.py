ba66f12add7b04a944660879f41d6a9e5c33a3bf
annabelle-josie <ajroxford@gmail.com>
2025-02-08 16:39:27 +0000

    Settings updated

--- data4good/trafficDisplay/views.py
+++ data4good/trafficDisplay/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
     return render(request, "index.html")
 
-def showStats(request):
-    return render(request, "tilly.html")
-
 def calculateRiskPage(request):
     return render(request, "calculate.html")
+
+def showStats(request):
+
+    return render(request, "tilly.html")
+
