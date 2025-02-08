7b9e4f3f3d40477473b424fd5fe0d9f786d2a51d
annabelle-josie <ajroxford@gmail.com>
2025-02-08 16:27:12 +0000

    Main 3 pages created

--- data4good/traffic/settings.py
+++ data4good/traffic/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "traffic.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+         'DIRS': ['trafficDisplay/templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "GMT"
 
 USE_I18N = True
 
