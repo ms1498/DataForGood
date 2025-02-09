d8aede702a4888ce746b26838340deea92e4c5f5
t5z4s6 <tillyzuzu@gmail.com>
2025-02-09 11:44:15 +0000

    a

--- data4good/trafficDisplay/views.py
+++ data4good/trafficDisplay/views.py
@@ -6,6 +6,9 @@ from incidence.incidence import find_incidence, get_location
 from TrafficAccidents.impact_score_impl import fetch_data
 
 def index(request):
+    df = filt.filter_data()
+    temp_df = df[~df['prim_contributory_cause'].isin(['NOT APPLICABLE', 'UNABLE TO DETERMINE'])]
+    top_10_causes = temp_df['prim_contributory_cause'].value_counts().index[:10][0].lower()
     # Get James's Number
     cenlat, cenlon = get_location()
 
@@ -16,14 +19,20 @@ def index(request):
     davidScores = fetch_data()
     
     overallScore =( max(davidScores[1], davidScores[2]) * likelihoodScore) *1.5
-    overallScore = ("{:.1f}".format(round(overallScore, 2)))
+    print( overallScore) 
     
+    if(overallScore<=100 and overallScore>=70): shouldDrive = "Please do not drive."
+    elif(overallScore<70 and overallScore>=30): shouldDrive = "You may be at a higher risk by driving so be careful."
+    else: shouldDrive = "You are safe to drive."
+
+    overallScore = ("{:.1f}".format(round(overallScore, 2)))
     plots = getPlots()
     context = {
         "plot1" : plots["plot1"],
         "plot2" : plots["plot2"],
         "plot3" : plots["plot3"],
-        "overallScore": overallScore
+        "overallScore": overallScore,
+        "recommendation" : f"{shouldDrive} Most people make mistakes by {top_10_causes}."
     }
     return render(request, "index.html", context)
 
@@ -62,6 +71,7 @@ def calculateRiskPage(request):
     else: shouldDrive = "You are safe to drive."
 
     overallScore = ("{:.1f}".format(round(overallScore, 2)))
+
     context = {
         "overallScore" : overallScore,
         "slightScore" : davidScores[0],
